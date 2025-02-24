11762fb5f2de005ab13e584295f1c8fc34d3d04a
Divyanshi-Joshi <142033456+Divyanshi-Joshi@users.noreply.github.com>
2025-02-25 00:27:40 +0530

    Update GameHeader.js

--- src/components/GameHeader.js
+++ src/components/GameHeader.js
@@ -1,59 +1,48 @@
 // src/components/GameHeader.js
+
 import React from 'react';
 import { FaHeart, FaCoins } from 'react-icons/fa';
 import sunImage from '../assets/sun.png';
-const GameHeader = ({ title, score, lives, coins }) => {
+import './GameHeader.css';
+
+const GameHeader = ({ title, score, lives, collectedCharacters, coins, gameMode }) => {
   return (
-    <header style={headerStyle}>
-      <div style={leftStyle}>
-        <div style={statStyle}>
-          <span>Score: {score}</span>
+    <header className="game-header">
+      <div className="header-left">
+        <div className="stat-container">
+          <span className="stat-label">Score:</span>
+          <span className="stat-value">{score}</span>
         </div>
-        <div style={statStyle}>
-          <FaHeart color="red" /> <span>{lives}</span>
+
+        <div className="stat-container lives-container">
+          {Array.from({ length: lives }, (_, index) => (
+            <FaHeart key={index} className="heart-icon" />
+          ))}
         </div>
-        <div style={statStyle}>
-          <FaCoins color="goldenrod" /> <span>{coins}</span>
-        </div>
+
+        {gameMode === 'world1' ? (
+          <div className="stat-container">
+            <span className="stat-label">Coins:</span>
+            <span className="stat-value">{coins}</span>
+            <FaCoins className="coin-icon" />
+          </div>
+        ) : (
+          <div className="stat-container">
+            <span className="stat-label">Collected:</span>
+            <span className="stat-value">{collectedCharacters}</span>
+          </div>
+        )}
       </div>
-      <div style={titleStyle}>
+
+      <div className="header-title">
         <h1>{title}</h1>
       </div>
-      <div style={rightStyle}>
-      <img src={sunImage} alt="Sun" style={{ width: '200px', height: '200px' }} />
+
+      <div className="header-right">
+        <img src={sunImage} alt="Sun" className="sun-image" />
       </div>
     </header>
   );
-};
-
-const headerStyle = {
-  padding: '10px 20px',
-  backgroundColor: '#87CEEB', // Sky blue color
-  color: 'white',
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-};
-
-const leftStyle = {
-  display: 'flex',
-  alignItems: 'center',
-};
-const rightStyle = {
-    display: 'flex',
-    alignItems: 'center',
-  };
-
-const statStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  marginRight: '20px',
-  fontSize: '18px',
-};
-
-const titleStyle = {
-  flex: 1,
-  textAlign: 'center',
 };
 
 export default GameHeader;
